79a19b8ee7f5a2bc1b85cb80beb34f27997de9cb
killjoy7788 <joethornton1992@gmail.com>
2018-02-01 16:40:14 -0800

    project created

---
+++ a1/src/a1/MyGame.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a1;
+
+import java.io.IOException;
+import ray.rage.Engine;
+import ray.rage.rendersystem.RenderWindow;
+import ray.rage.scene.SceneManager;
+
+/**
+ *
+ * @author Joe
+ */
+public class MyGame extends ray.rage.game.VariableFrameRateGame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    @Override
+    protected void update(Engine engine) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void setupCameras(SceneManager sm, RenderWindow rw) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void setupScene(Engine engine, SceneManager sm) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
