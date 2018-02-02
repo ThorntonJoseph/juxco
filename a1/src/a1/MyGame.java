ae69fe1e1a75840573061099f62516f423d6feb8
killjoy7788 <joethornton1992@gmail.com>
2018-02-01 17:06:56 -0800

    start

--- a1/src/a1/MyGame.java
+++ a1/src/a1/MyGame.java
@@ -4,23 +4,56 @@
  * and open the template in the editor.
  */
 package a1;
-
+import ray.rage.game.*;
 import java.io.IOException;
 import ray.rage.Engine;
 import ray.rage.rendersystem.RenderWindow;
 import ray.rage.scene.SceneManager;
+import ray.rage.rendersystem.gl4.GL4RenderSystem;
+import ray.rage.scene.Camera;
+import ray.rage.scene.*;
 
 /**
  *
  * @author Joe
  */
-public class MyGame extends ray.rage.game.VariableFrameRateGame{
-
+public class MyGame extends VariableFrameRateGame{
+    GL4RenderSystem rs;
+    float etime;
+    String score;
+    
+    public MyGame(){
+        super();
+        // moves the camera arround
+        System.out.println("Movement keys keys");
+        System.out.println("\t press A to move left");
+        System.out.println("\t press D to move right");
+        System.out.println("\t press S to move backward");
+        System.out.println("\t press W to move forward");
+        System.out.println("\t press spacebar to get on and off the dolphin");
+        System.out.println("Orientation keys");
+        System.out.println("\t press left arrow to rotate left arround vertical axis");
+        System.out.println("\t press right arrow to rotate right arround vertical axis");
+        System.out.println("\t press down arrow to rotate down arround horizontal axis");
+        System.out.println("\t press up arrow to rotate up arround horizontal axis");
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Game game = new MyGame();
+        try{
+            game.startup();
+            game.run();
+        }
+        catch(Exception e){
+            e.printStackTrace(System.err);
+        }
+        finally
+        {
+            game.shutdown();
+            game.exit();
+        }
     }
 
     @Override
@@ -30,7 +63,11 @@ public class MyGame extends ray.rage.game.VariableFrameRateGame{
 
     @Override
     protected void setupCameras(SceneManager sm, RenderWindow rw) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        SceneNode rootNode = sm.getRootSceneNode();
+        Camera camera = sm.createCamera("Player camera", Camera.Frustum.Projection.PERSPECTIVE);
+        rw.getViewport(0).setCamera(camera);
+        SceneNode cameraNode = rootNode.createChildSceneNode(camera.getName()+"Node");
+        cameraNode.attachObject(camera);
     }
 
     @Override
